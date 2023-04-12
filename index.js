275e5f30a33ad9a5ee7d7115969a26689482428b
Christoph Groll <c.groll@hotmail.de>
2023-04-12 09:34:11 +0200

    added heartbeat and polling

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const request = require('request');
 
+const envMode = process.env.mode || "once";
 const envHeaders = process.env.headers||"";
 const envUrl = process.env.url;
 const envMethod = process.env.method||"GET";
 const envBody = process.env.body||"";
+const hearthBeatUrl = process.env.hearthBeatUrl||null;
 
 if(envUrl==null) throw new Error('url required!');
 
@@ -26,8 +28,51 @@ function parseEnvObject(env){
     return newObject;
 }
 
-if(envMethod=='GET'){
-    request.get(envUrl, {maxRedirects:0, headers:parseEnvObject(envHeaders)}, (error, response, body)=>{
+var counter = 1;
+
+function handleProcess(){
+    console.log("starting requires", counter);
+    counter++;
+    /* Only Once */
+    if(envMethod=='GET'){
+        request.get(envUrl, {maxRedirects:0, headers:parseEnvObject(envHeaders)}, (error, response, body)=>{
+            if(error){
+                console.log(error);
+                process.exit(1);
+            }else{
+                console.log(response.statusCode);
+                console.log(body);
+                if(response.statusCode!=200){
+                    throw new Error(`statusCode ${response.statusCode}`)
+                }
+                sendHearthBeat();
+            }
+        })
+    }else{
+        request.post(envUrl, {maxRedirects:0, headers:parseEnvObject(envHeaders), body:JSON.stringify(parseEnvObject(env))}, (error, response, body)=>{
+            if(error){
+                console.log(error);
+                process.exit(1);
+            }else{
+                console.log(response.statusCode);
+                console.log(body);
+                if(response.statusCode!=200){
+                    throw new Error(`statusCode ${response.statusCode}`)
+                }
+                sendHearthBeat();
+            }
+        });
+    }
+}
+
+handleProcess();
+
+
+
+
+function sendHearthBeat(){
+    if(hearthBeatUrl==null) return;
+    request.get(hearthBeatUrl, {}, (error, response, body)=>{
         if(error){
             console.log(error);
             process.exit(1);
@@ -35,21 +80,14 @@ if(envMethod=='GET'){
             console.log(response.statusCode);
             console.log(body);
             if(response.statusCode!=200){
-                throw new Error(`statusCode ${response.statusCode}`)
+                throw new Error(`hearthbeat statusCode ${response.statusCode}`)
             }
+        }
+        if(envMode=="poll"){
+            console.log("waiting for next cycle in 5000ms")
+            setTimeout(()=>{
+                handleProcess();
+            }, 5000)
         }
     })
-}else{
-    request.post(envUrl, {maxRedirects:0, headers:parseEnvObject(envHeaders), body:JSON.stringify(parseEnvObject(env))}, (error, response, body)=>{
-        if(error){
-            console.log(error);
-            process.exit(1);
-        }else{
-            console.log(response.statusCode);
-            console.log(body);
-            if(response.statusCode!=200){
-                throw new Error(`statusCode ${response.statusCode}`)
-            }
-        }
-    });
 }
